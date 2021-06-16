fc607efba3bbcd09eb689de7ba2317c146e859ab
zoeyzhao <zoeyzhao19@gmail.com>
2021-06-16 11:03:46 +0800

    build: first build

---
+++ README.md
@@ -0,0 +1,13 @@
+## install cli
+```
+npm link
+```
+
+### install project
+```
+engineer create [project-name]
+
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ api/process.js
@@ -0,0 +1,11 @@
+module.exports.spawn = async (...args) => {
+  const { spawn } = require('child_process')
+  return new Promise(resolve => {
+    const proc = spawn(...args)
+    proc.stdout.pipe(process.stdout)
+    proc.stderr.pipe(process.stderr)
+    proc.on('close', () => {
+      resolve()
+    })
+  })
+ }
